c71ffd8b808a76a1fc50aa981ae973df79355bf9
unknown <loganverghese@gmail.com>
2021-07-11 23:19:11 -0400

    questions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-var highScores = [];
+var questions = [
+    {
+        quetitle: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+
+    {
+        quetitle: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+
+    {
+        quetitle: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        quetitle: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+
+];
